06c73d59c0ff356ca2771eb0299e72b02c1342fc
Arjun Venkataswamy <avenkat2@gmail.com>
2018-11-13 21:16:10 -0600

    remove old migration

--- db/migrate/20181112075606_add_fields_to_section.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToSection < ActiveRecord::Migration[5.2]
-  def change
-    add_column :sections, :day, :string
-    add_column :sections, :start_time, :time
-    add_column :sections, :end_time, :time
-  end
-end
