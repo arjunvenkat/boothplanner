36cc7af002f80607bb4d7f756131176e0ae29748
Stephen Puiszis <stephen.puiszis@gmail.com>
2021-03-03 10:52:28 -0500

    Cleanup schema for unused tables and column migration error

--- db/migrate/20181112054235_rename_enrollment_column_in_sections.rb
+++ db/migrate/20181112054235_rename_enrollment_column_in_sections.rb
@@ -1,6 +1,5 @@
 class RenameEnrollmentColumnInSections < ActiveRecord::Migration[5.2]
   def change
-    remove_column :sections, :phase_4_total_enrollment, :integer
     add_column :sections, :phase_4_enrollment, :integer
   end
 end
